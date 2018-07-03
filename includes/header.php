0f5ca5b15f554cedca5c563fcbe94f6060abd595
Mhandie <anokpara@yahoo.com>
2018-07-03 14:35:46 +0100

    First commit

--- includes/header.php
+++ includes/header.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>This is my PHP site</title>
-	<link rel="stylesheet" type="text/css" href="style/core.css">
-	<link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
+  <title>mhandie.com</title>
+  <link rel="stylesheet" type="text/css" href=" style/core.css">
 </head>
-  <body class="text-center">
-	<header>
-		<h1>Amanda Okpara</h1>
-		<nav>
-	<ul>
-	<li><a href="index.php">Home</a></li>
-    <li><a href="index.php?about">About</a></li>
-	<li><a href="index.php?product">Product</a></li>
-    <li><a href="index.php?blog">Blog</a></li>
-	<li><a href="index.php?contact">Contact</a></li>
-   </ul>
-		</nav>
-	</header>
+<body>
+
+ <header>
+   <h1>Ashkorleshionz</h1>
+   <nav>
+     <ul>
+    <li><a href="index.php">Home</a></li>
+       <li><a href="index.php?about">About</a></li>
+       <li><a href="index.php?products">Products</a></li>
+       <li><a href="index.php?blog">Blog</a></li>
+       <li><a href="index.php?contact">Contact </a></li>
+
+     </ul>
+   </nav>
+ </header>
